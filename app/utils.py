ab1c3a4f4302cd2f57b84ab18ef293cb0636a979
sxntiagoad <salvarezd3@eafit.edu.co>
2025-01-04 20:11:31 -0500

    debug version

--- app/utils.py
+++ app/utils.py
@@ -4,24 +4,24 @@ from app import db
 
 def can_register(ip_address, device_info):
     """Check if registration is allowed based on IP and device"""
-    two_days_ago = datetime.utcnow() - timedelta(days=2)
+    # two_days_ago = datetime.utcnow() - timedelta(days=2)
     
-    # Check IP address
-    ip_registration = User.query.filter(
-        User.ip_address == ip_address,
-        User.created_at > two_days_ago
-    ).first()
+    # # Check IP address
+    # ip_registration = User.query.filter(
+    #     User.ip_address == ip_address,
+    #     User.created_at > two_days_ago
+    # ).first()
     
-    if ip_registration:
-        return False
+    # if ip_registration:
+    #     return False
     
-    # Check device info
-    device_registration = User.query.filter(
-        User.device_info == device_info,
-        User.created_at > two_days_ago
-    ).first()
+    # # Check device info
+    # device_registration = User.query.filter(
+    #     User.device_info == device_info,
+    #     User.created_at > two_days_ago
+    # ).first()
     
-    if device_registration:
-        return False
+    # if device_registration:
+    #     return False
     
     return True
